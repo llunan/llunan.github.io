f84c2ffa2816a4278b9bc96f0a0d25ed66dcf084
llunan <104824075+llunan@users.noreply.github.com>
2022-08-08 23:24:57 +0800

    Add files via upload

--- 2022_national_day_holiday_countdown/main.js
+++ 2022_national_day_holiday_countdown/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("October 01, 2022 00:00:00").getTime();
+var countDownDate = new Date("September 10, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -25,7 +25,7 @@ var x = setInterval(function() {
 
   document.getElementById("demo4").innerHTML = seconds ;
     
-  // If the count down is over, write some text 
+  // If the countdown is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
